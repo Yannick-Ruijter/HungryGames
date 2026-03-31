d7f841247fa14fd900bc20e65ce049a6b9ab9dc1
rentz <fv_intheinn@icloud.com>
2026-03-31 15:11:52 +0200

    adding npc ai script

---
+++ HungryGames/Assets/Scripts/NPC/NPC_AI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NPC_AI : MonoBehaviour
+{
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // start of frame, enabl nav agent. get stuff. diable nav agent.
+        // con
+    }
+}
