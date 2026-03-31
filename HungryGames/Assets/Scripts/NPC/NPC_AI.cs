312e32aa5721696757505666f57cba52eb31830a
rentz <fv_intheinn@icloud.com>
2026-03-31 15:57:40 +0200

    adding test scene with nav mesh

--- HungryGames/Assets/Scripts/NPC/NPC_AI.cs
+++ HungryGames/Assets/Scripts/NPC/NPC_AI.cs
@@ -1,14 +1,18 @@
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NPC_AI : MonoBehaviour
 {
-
-
-
+    [SerializeField] private NavMeshAgent _navAgent;
+    //[SerializeField] private NavMeshSurface _navSurface;
+    private Vector3 _velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        NavMeshHit hit;
+        NavMesh.SamplePosition(new Vector3(1, 0, 1), out hit, 1.0f, NavMesh.AllAreas);
+        _navAgent.destination = hit.position;
     }
 
     // Update is called once per frame
@@ -16,5 +20,18 @@ public class NPC_AI : MonoBehaviour
     {
         // start of frame, enabl nav agent. get stuff. diable nav agent.
         // con
+        _navAgent.enabled = true;
+        //get positionn and keep between 0, 1
+        //NavMeshHit hit;
+        //NavMesh.SamplePosition(new Vector3(1,0,1), out hit, 1.0f, NavMesh.AllAreas);
+        //_navAgent.destination = hit.position;
+        _velocity = _navAgent.velocity;
+        if(_velocity.magnitude > 1)
+        {
+            _velocity.Normalize();
+        }
+        
+        transform.position += _velocity;
+        _navAgent.enabled = false;
     }
 }
