4635b96d7dd5d888a9faa52abd0e25261d4461a7
rentz <fv_intheinn@icloud.com>
2026-04-02 12:06:30 +0200

    build bugs

--- HungryGames/Assets/Scripts/Managers/MainMenuManager.cs
+++ HungryGames/Assets/Scripts/Managers/MainMenuManager.cs
@@ -10,7 +10,6 @@ public class MainMenuManager : MonoBehaviour
 
     public void Quit()
     {
-        if(Application.isEditor) UnityEditor.EditorApplication.isPlaying = false;
-        else Application.Quit();
+        Application.Quit();
     }
 }
